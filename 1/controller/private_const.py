ae9f8df872273a25c3de7745e971ca5b5385299c
gzb1985 <gzb1985@gmail.com>
2012-09-26 22:23:45 +0800

    修改防止私密常量位置

---
+++ 1/controller/private_const.py
@@ -0,0 +1,6 @@
+
+redirect_url_weibo = 'http://weiboapidebugger.sinaapp.com/weibo' #your weibo auth redirect url
+weibo_appid = ''  #your weibo app id
+weibo_app_secret='' #your weibo app secret
+
+beaker_session_validate_key='12345'  #your beaker_session_validate_key, '12345' for example
